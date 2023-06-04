a488552e6574633d0cdda876ee3df00c6fbca97d
Alex de la Rosa <alexdelarosacortes@gmail.com>
2023-06-04 16:35:29 -0600

    Initial Commit

---
+++ tictactoe/tictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoeApp.swift
+//  tictactoe
+//
+//  Created by Alex de la Rosa on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct tictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
